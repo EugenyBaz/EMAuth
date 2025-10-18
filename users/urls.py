1efbec336bebed34c223ec340c0210543c840f37
Bazavod <eugeny.bazavod@list.ru>
2025-10-18 16:02:34 +0300

    update path home page

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 # from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from users.views import RegistrationView, UserViewSet
+
+def home(request):
+    return HttpResponse("<h1>Добро пожаловать!</h1>")
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path("", home, name='home'),
+    path("", include(router.urls)),
     path("register/", RegistrationView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
 ]
